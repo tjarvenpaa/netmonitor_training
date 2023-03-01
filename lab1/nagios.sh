d13eeddcdc0620b9f1d032621cc2c01ba138a1bd
rikeardi <risto.lievonen@gmail.com>
2023-03-01 23:44:24 +0200

    ok

--- lab1/nagios.sh
+++ lab1/nagios.sh
@@ -4,3 +4,4 @@ ip addr add 10.10.20.2/24 dev eth1
 ip route add 1.1.1.0/24 via 10.10.20.1
 
 echo "cfg_file=/opt/nagios/etc/objects/router.cfg" >> /opt/nagios/etc/nagios.cfg
+/opt/nagios/bin/nagios -d /opt/nagios/etc/nagios.cfg
