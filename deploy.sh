13f0cf15f4c4a25a0b96efc74b5600e0e0d8590b
rikeardi <risto.lievonen@gmail.com>
2023-03-01 22:18:31 +0200

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+lab = $1
 
 docker run --rm -it --privileged --network host \
      -v /var/run/docker.sock:/var/run/docker.sock \
@@ -6,6 +7,6 @@ docker run --rm -it --privileged --network host \
      -v /etc/hosts:/etc/hosts \
      -v /var/lib/docker/containers:/var/lib/docker/containers \
      --pid="host" \
-     -v $(pwd)/lab$1:$(pwd) \
-     -w $(pwd)/lab$1 \
+     -v $(pwd)/${lab}:$(pwd) \
+     -w $(pwd)/${lab} \
      ghcr.io/srl-labs/clab clab deploy
