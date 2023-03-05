6844c1d0bc3121afcdabfc770def2ed7ba6c6d94
rikeardi <risto.lievonen@gmail.com>
2023-03-05 13:25:50 +0200

    observium

--- lab2/observium.sh
+++ lab2/observium.sh
@@ -3,6 +3,8 @@
 ip addr add 10.10.20.2/24 dev eth1
 ip route add 1.1.1.0/24 via 10.10.20.1
 
+/opt/observium/adduser.php lab2 lab2 10
+
 /opt/observium/add_device.php 1.1.1.1 lab2
 /opt/observium/add_device.php 1.1.1.2 lab2
 /opt/observium/add_device.php 1.1.1.3 lab2
