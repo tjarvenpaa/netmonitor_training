435b1aacfb3e680099a423f35dc37ae307d7aa47
rikeardi <risto.lievonen@gmail.com>
2023-03-05 19:52:49 +0200

    lab3

--- lab3/router.sh
+++ lab3/router.sh
@@ -5,9 +5,8 @@ apt-get update
 apt-get install frr-snmp -y
 N
 
-adduser --quiet --system --group --home $SNMPDIR --shell /usr/sbin/nologin Debian-snmp
+#adduser --quiet --system --group --home $SNMPDIR --shell /usr/sbin/nologin Debian-snmp
 
 service snmpd start
 
-sleep 10
 /usr/lib/frr/frr-reload
