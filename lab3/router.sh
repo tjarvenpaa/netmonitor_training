eae3dfb94d27e4041a1c58fd2e479dcd752e2bab
rikeardi <risto.lievonen@gmail.com>
2023-03-05 15:56:20 +0200

    lab3 snmp

--- lab3/router.sh
+++ lab3/router.sh
@@ -5,5 +5,10 @@ apt-get update
 apt-get install frr-snmp -y
 N
 
+rm -rf /etc/snmp/snmpd.conf
+mv /etc/snmp/snmpd.conf.new /etc/snmp/snmpd.conf
+
+service snmpd start
+
 sleep 10
 /usr/lib/frr/frr-reload
