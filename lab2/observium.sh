60510c9f657b0a22a6e9a31347e46fa34090bbee
rikeardi <risto.lievonen@gmail.com>
2023-03-05 13:24:28 +0200

    observium

--- lab2/observium.sh
+++ lab2/observium.sh
@@ -3,5 +3,8 @@
 ip addr add 10.10.20.2/24 dev eth1
 ip route add 1.1.1.0/24 via 10.10.20.1
 
-#echo "cfg_file=/opt/nagios/etc/objects/router.cfg" >> /opt/nagios/etc/nagios.cfg
-#/opt/nagios/bin/nagios -d /opt/nagios/etc/nagios.cfg
+/opt/observium/add_device.php 1.1.1.1 lab2
+/opt/observium/add_device.php 1.1.1.2 lab2
+/opt/observium/add_device.php 1.1.1.3 lab2
+
+/opt/observium/poller.php -h 1.1.1.*
