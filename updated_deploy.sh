f124d344a6cd9c51a9cb6060fa8e8a593ded4eb3
Teemu Järvenpää <teemu.jarvenpaa@hamk.fi>
2024-12-20 10:01:31 +0200

    Create updated_deploy.sh
    using new version of containerlab image, usable with lab3

---
+++ updated_deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd $1
+
+docker run --rm -it --privileged --network host \
+     -v /var/run/docker.sock:/var/run/docker.sock \
+     -v /var/run/netns:/var/run/netns \
+     -v /etc/hosts:/etc/hosts \
+     -v /var/lib/docker/containers:/var/lib/docker/containers \
+     --pid="host" \
+     -v $(pwd):$(pwd) \
+     -w $(pwd) \
+     ghcr.io/srl-labs/clab:0.60.1 clab deploy
+
+cd ..
